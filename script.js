f23c44af49d377051f6845c3d8b83f23ff863636
WaliaMazhar <farzak2520@gmail.com>
2024-12-23 16:57:29 +0500

    nintendo is a gaming website

---
+++ script.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    },
+    loop: true,
+
+    margin: 10,
+
+    dots: true
+})
+
+
